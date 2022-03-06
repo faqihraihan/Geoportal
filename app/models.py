f9d057490d7122ec2592c79b891788bf5f4a72ff
Muh. Faqih Raihan <faqihraihan.akarbadi@gmail.com>
2022-03-06 16:44:40 +0800

    Penyempurnaan file website

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.VARCHAR(200), unique=True)
+    password = db.Column(db.VARCHAR(200))
+    nama = db.Column(db.VARCHAR(200))
