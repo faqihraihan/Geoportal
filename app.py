51d7e63a864cfb3eff5e984709895cce2e535d59
Lidyah <51530984+Lidyah@users.noreply.github.com>
2022-04-01 14:30:31 +0800

    Update Main

---
+++ app.py
@@ -0,0 +1,11 @@
+from app import app
+
+# python -m venv env
+
+# env\scripts\activate
+# set FLASK_APP=app.py
+# set FLASK_DEBUG=1
+# flask run
+
+# pip freeze > requirements.txt
+# pip install -r requirements.txt
---
+++ app/models.py
@@ -0,0 +1,43 @@
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.VARCHAR(200), unique=True)
+    password = db.Column(db.VARCHAR(200))
+    nama = db.Column(db.VARCHAR(200))
+    nohp = db.Column(db.VARCHAR(200))
+    lvl = db.Column(db.Integer)
+    img = db.Column(db.LargeBinary(length=(2**32)-1))
+
+class Provinsi(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nama = db.Column(db.VARCHAR(200))
+    kab = db.relationship("Kabupaten", backref="provinsi")
+
+class Kabupaten(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_prov = db.Column(db.Integer, db.ForeignKey('provinsi.id'))
+    nama = db.Column(db.VARCHAR(200))
+    kec = db.relationship("Kecamatan", backref="kabupaten")
+
+class Kecamatan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_kab = db.Column(db.Integer, db.ForeignKey('kabupaten.id'))
+    nama = db.Column(db.VARCHAR(200))
+    desa = db.relationship("Desa", backref="kecamatan")
+
+class Desa(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    id_kec = db.Column(db.Integer, db.ForeignKey('kecamatan.id'))
+    nama = db.Column(db.VARCHAR(200))
+
+class Kelompok_Tani(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    nama = db.Column(db.VARCHAR(200))
+    no_sk = db.Column(db.VARCHAR(200)) 
+
+class Peta_Desa(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    nama = db.Column(db.VARCHAR(200))
+    json = db.Column(db.JSON) 
